e78870132edbc648514ec8e06f20a2e1f3168b8e
SomeBoringNerd <dev.someboringnerd@protonmail.com>
2023-02-07 11:29:50 +0100

    Logger pour le client QT (système de log standardisé entre les différents logiciels)

--- Projet-IHM-Plaque/mainwindow.cpp
+++ Projet-IHM-Plaque/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "logger.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    initLogger("ClientQT");
 }
 
 MainWindow::~MainWindow()
